9c75b65ae5ea2eb6930cb6acd96f10d74dc550aa
axelhult <axelhult3@gmail.com>
2019-07-14 10:40:57 +0200

    Add project files.

---
+++ TutorialsPointCSharp/DataTypesClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelperClasses
+{
+    public static class DataTypesClass
+    {
+        static int x;
+
+        public static void printDataTypes()
+        {
+            Console.WriteLine("int is size:" + x);
+        }
+    }
+}
---
+++ TutorialsPointCSharp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+ 
+namespace TutorialsPointCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelperClasses.DataTypesClass.printDataTypes();
+
+
+            //dynamic x, y, z;
+            int x = 3;
+            Double y = 15.99;
+
+            dynamic z = y / x;
+
+            Console.WriteLine(x.GetType());
+            Console.WriteLine(y.GetType());
+            Console.WriteLine(z.GetType());
+            Console.ReadLine();
+        }
+    }
+}
