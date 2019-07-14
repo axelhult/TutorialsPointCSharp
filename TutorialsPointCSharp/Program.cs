5677e706afdf3462a029210b3105a60ba29e222e
axelhult <axelhult3@gmail.com>
2019-07-14 10:43:56 +0200

    tog bort gammalt skit

--- TutorialsPointCSharp/Program.cs
+++ TutorialsPointCSharp/Program.cs
@@ -8,16 +8,8 @@ namespace TutorialsPointCSharp
         {
             HelperClasses.DataTypesClass.printDataTypes();
 
-
-            //dynamic x, y, z;
-            int x = 3;
-            Double y = 15.99;
-
-            dynamic z = y / x;
-
-            Console.WriteLine(x.GetType());
-            Console.WriteLine(y.GetType());
-            Console.WriteLine(z.GetType());
+;
+            Console.WriteLine();
             Console.ReadLine();
         }
     }
